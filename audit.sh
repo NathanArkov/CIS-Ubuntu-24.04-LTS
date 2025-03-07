3fcedaadb290a1e57e691983487297b27eda094b
Nathan Arkov <90056309+NathanArkov@users.noreply.github.com>
2025-03-07 13:52:04 +0000

    {message}

--- audit.sh
+++ audit.sh
@@ -3,6 +3,7 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 FIREWALL="$(dpkg -l |awk '/^[hi]i/{print $2}' | grep -E 'iptables|nftables|ufw')"
+OK=0
 
 
 # Function to display section headers
@@ -29,23 +30,29 @@ check_filesystem_kernel_modules() {
             # echo "Checking $module module..."
             if grep -qE "/bin/true|/bin/false" /etc/modprobe.d/*; then
                 print_success "$module has /bin/true or /bin/false entry"
+                OK+=1
+
+
             else
                 print_error "$module does not have /bin/true or /bin/false entry"
             fi
 
             if grep -q "blacklist $module" /etc.modprobe.d/*; then
                 print_success "$module is deny listed"
+                OK+=1
             else
                 print_error "$module is not deny listed"
             fi
 
             if ! lsmod | grep -q "$module"; then
                 print_success "$module is not loaded in the running kernel"
+                OK+=1
             else
                 print_error "$module is loaded in the running kernel"
             fi
         else
             print_success "$module module is not available or pre-compiled into the kernel"
+            OK+=1
         fi
     done
 }
@@ -56,6 +63,7 @@ check_ptrace_scope() {
     ptrace_scope=$(cat /proc/sys/kernel/yama/ptrace_scope)
     if [[ "$ptrace_scope" -ge 1 && "$ptrace_scope" -le 3 ]]; then
         print_success "ptrace_scope is restricted"
+        OK+=1
     else
         print_error "ptrace_scope is not restricted"
     fi
@@ -66,6 +74,7 @@ check_core_dumps() {
     print_header "1.5.3 Core dumps restricted"
     if sysctl fs.suid_dumpable | grep -q "0"; then
         print_success "Core dumps are restricted"
+        OK+=1
     else
         print_error "Core dumps are not restricted"
     fi
@@ -76,6 +85,7 @@ check_prelink() {
     print_header "1.5.4 prelink"
     if ! command -v prelink &> /dev/null; then
         print_success "prelink is uninstalled"
+        OK+=1
     else
         print_error "prelink is installed"
     fi
@@ -88,6 +98,7 @@ check_error_reporting() {
         print_error "Automatic error reporting is enabled"
     else
         print_success "Automatic error reporting is not enabled"
+        OK+=1
     fi
 }
 
@@ -96,24 +107,28 @@ check_warning_banners() {
     print_header "1.6 Command line warning banners"
     if [ -f /etc/motd ]; then
         print_success "MOTD is configured"
+        OK+=1
     else
         print_error "MOTD is not configured"
     fi
 
     if [ -f /etc/issue ]; then
         print_success "Local login warning banner is configured"
+        OK+=1
     else
         print_error "Local login warning banner is not configured"
     fi
 
     if [ -f /etc/issue.net ]; then
         print_success "Remote login warning banner is configured"
+        OK+=1
     else
         print_error "Remote login warning banner is not configured"
     fi
 
     if ls -l /etc/motd /etc/issue /etc/issue.net | grep -q "root"; then
         print_success "Access to /etc/motd, /etc/issue, and /etc/issue.net is configured"
+        OK+=1
     else
         print_error "Access to /etc/motd, /etc/issue, and /etc/issue.net is not configured"
     fi
